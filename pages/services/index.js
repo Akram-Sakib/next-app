e082c1a9a8aa80b9ea58b2b83d944bb16a15a86b
Akram-Sakib <sayedakramsakib@gmail.com>
2021-12-30 22:02:16 +0600

    initial

---
+++ pages/services/index.js
@@ -0,0 +1,7 @@
+const service = () => {
+    return ( 
+        <h2>This is Main Service Page</h2>
+     );
+}
+ 
+export default service;
---
+++ pages/services/services.js
@@ -0,0 +1,9 @@
+const Services = () => {
+    return ( 
+        <div>
+            <h2>Service Page</h2>
+        </div>
+     );
+}
+ 
+export default Services;
